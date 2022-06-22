ca6cf4460527f954c78269e1f4d08c5cecf7b2f9
allx-under <95172339+allx-under@users.noreply.github.com>
2022-06-22 22:26:57 +0300

    add hw11

--- src/index.js
+++ src/index.js
@@ -8,66 +8,97 @@ const KEY = "28193181-1d0a0826250053d79f38b5461";
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
-    gallery: document.querySelector('.gallery')
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn:document.querySelector('.load-more')
 }
 
-
+let page = 1;
+let searchContent = "";
 
 refs.searchForm.addEventListener('submit', inInputSearch)
+refs.loadMoreBtn.addEventListener('click', showMoreOnClick)
 
 async function getData(content) {
 
-    const response = await axios.get(`${BASE_URL}/?key=${KEY}&q=${content}&image_type=photo&orientation=horizontal&safesearch=true`);
+    const response = await axios.get(`${BASE_URL}/?key=${KEY}&q=${content}&image_type=photo&orientation=horizontal&safesearch=true&&page=${page}&per_page=40`);
     return response;
     
 }
 
 async function inInputSearch(e) {
     e.preventDefault();
-    const searchContent = e.target.elements.searchQuery.value;
+    resetIfNewInput(e);
+
+    searchContent = e.target.elements.searchQuery.value;
+    
     try {
         const { data } = await getData(searchContent);
-
-        const galleryItems = data.hits.map(hit => {
-            const {webformatURL,largeImageURL,tags, likes,views,comments,downloads } = hit;
-           return `<div class="photo-card">
-                <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-                <div class="info">
-                <p class="info-item">
-                <b>Likes${likes}</b>
-                </p>
-                <p class="info-item">
-                <b>Views${views}</b>
-                </p>
-                <p class="info-item">
-                <b>Comments${comments}</b>
-                </p>
-                <p class="info-item">
-                <b>Downloads${downloads}</b>
-                </p>
-                </div>
-                </div>`
+        renderResults(data);
+         if (data.hits.length === 0) {
+        return Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         }
-        ).join('');
-        refs.gallery.innerHTML = galleryItems;
-        let gallery = new SimpleLightbox('.photo-card a');
-        showMsgInCaseNoResults(data.hits.length)
-
+        refs.loadMoreBtn.classList.remove('is-hidden');
+        addSimpleLigthBox();
     } catch (error) {
         console.log(error)
         
     }
     refs.searchForm.reset();
+    page += 1;
 }
 
-function renderResults() {
+async function showMoreOnClick() {
+    try {
+        const { data } = await getData(searchContent);
+        renderResults(data);
+        addSimpleLigthBox()
+        if (data.hits.length < 40) {
+            refs.loadMoreBtn.classList.add('is-hidden');
+            Notify.failure("We're sorry, but you've reached the end of search results.")
+        }
+        page += 1;
     
+    } catch (error) {
+        console.log(error)
+        
+    }
 }
 
-function showMsgInCaseNoResults(array) {
-    if (array === 0) {
-            Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+function renderResults(data) {
+    const galleryItems = data.hits.map(hit => {
+            const {webformatURL,largeImageURL,tags, likes,views,comments,downloads } = hit;
+           return `<div class="photo-card">
+                <a href="${largeImageURL}"><img class="photo-img" src="${webformatURL}" alt="${tags}" loading="lazy" width=300px height=280px /></a>
+                <div class="info">
+                <p class="info-item">
+                <b>Likes</b>${likes}
+                </p>
+                <p class="info-item">
+                <b>Views</b>${views}
+                </p>
+                <p class="info-item">
+                <b>Comments</b>${comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads</b>${downloads}
+                </p>
+                </div>
+                </div>`
+        }
+        ).join('');
+        refs.gallery.insertAdjacentHTML('beforeend',galleryItems);
+}
+
+function resetIfNewInput(event) {
+    if (searchContent !== event.target.elements.searchQuery.value) {
+        page = 1;
+        refs.gallery.innerHTML = "";
+        refs.loadMoreBtn.classList.add('is-hidden')
     }
+}
+
+function addSimpleLigthBox() {
+    let gallery = new SimpleLightbox('.photo-card a')
 }
 
 
